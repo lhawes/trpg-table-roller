aa8658e100dbb013ce55a6e5d53fc628266e5813
lhawes <lucashawes@Lucass-MacBook-Pro.local>
2021-01-03 22:50:59 -0800

    add build to project

---
+++ precache-manifest.169eb13dbb8d94deb36587ad4e347a5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe96fc29e9f6c60cf535a6c88963f68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1745c318733947101e66",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cabd98a3df53f4dae855",
+    "url": "/static/js/2.8aa1c046.chunk.js"
+  },
+  {
+    "revision": "3621bf018cdcd6b92450",
+    "url": "/static/js/3.d1fda965.chunk.js"
+  },
+  {
+    "revision": "1745c318733947101e66",
+    "url": "/static/js/main.6a2e77ea.chunk.js"
+  },
+  {
+    "revision": "e6c46f003f9f99b2ebd9",
+    "url": "/static/js/runtime-main.67d0b362.js"
+  }
+]);
