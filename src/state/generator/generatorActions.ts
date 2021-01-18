dd1886cc6222fa384bd97bf7abbfafca7e67dbac
lhawes <lucashawes@Lucass-MacBook-Pro.local>
2021-01-18 14:22:50 -0800

    add operational actions and action types

--- src/state/generator/generatorActions.ts
+++ src/state/generator/generatorActions.ts
@@ -3,101 +3,104 @@ import { RPGGenerator } from "../../types/Generator";
 import { Table } from "../../types/Table";
 import { GeneratorActionTypes } from "./generatorActionTypes";
 
-export const addTableAction = (table: Table):AnyAction => {
-  return {
+export const addTableAction = (table: Table):AnyAction => ({
     type: GeneratorActionTypes.ADD_TABLE,
     payload: table,
-  }
-}
+})
 
-export const removeTableAction = (tableIndex: number): AnyAction => {
-  return {
+export const removeTableAction = (tableIndex: number): AnyAction => ({
     type: GeneratorActionTypes.REMOVE_TABLE,
     payload: {
       tableIndex
     }
-  }
-}
+})
 
-export const addTableEntryAction = (tableEntry: string, tableIndex: number): AnyAction => {
-  return {
+export const addTableEntryAction = (tableEntry: string, tableIndex: number): AnyAction => ({
     type: GeneratorActionTypes.ADD_TABLE_ENTRY,
     payload: {
       tableIndex,
       tableEntry
     }
-  }
-}
+})
 
-export const removeTableEntryAction = (tableIndex: number, tableEntryIndex: number): AnyAction => {
-  return {
+export const removeTableEntryAction = (tableIndex: number, tableEntryIndex: number): AnyAction => ({
     type: GeneratorActionTypes.REMOVE_TABLE_ENTRY,
     payload: {
       tableIndex,
       tableEntryIndex
     }
-  }
-}
+})
 
-export const changeTableEntryAction = (tableEntry: string, tableIndex: number, tableEntryIndex: number): AnyAction => {
-  return {
+export const changeTableEntryAction = (tableEntry: string, tableIndex: number, tableEntryIndex: number): AnyAction => ({
     type: GeneratorActionTypes.CHANGE_TABLE_ENTRY,
     payload: {
       tableEntry,
       tableIndex,
       tableEntryIndex,
     }
-  }
-}
+})
 
-export const changeGeneratorNameAction = (generatorName: string): AnyAction => {
-  return {
+export const changeGeneratorNameAction = (generatorName: string): AnyAction => ({
     type: GeneratorActionTypes.CHANGE_GENERATOR_NAME,
     payload: {
       generatorName
     }
-  }
-}
+})
 
-export const changeTextTemplateAction = (textTemplate: string): AnyAction => {
-  return {
+export const changeTextTemplateAction = (textTemplate: string): AnyAction => ({
     type: GeneratorActionTypes.CHANGE_TEXT_TEMPLATE,
     payload: {
       textTemplate
     }
-  }
-}
+})
 
-export const changeTableNameAction = (tableName: string, tableIndex: number): AnyAction => {
-  return {
+export const changeTableNameAction = (tableName: string, tableIndex: number): AnyAction => ({
     type: GeneratorActionTypes.CHANGE_TABLE_NAME,
     payload: {
       tableName,
       tableIndex
     }
-  }
-}
+})
 
-export const updateStateFromFileAction = (fileState: Record<string, RPGGenerator>) => {
-  return {
+export const updateStateFromFileAction = (fileState: Record<string, RPGGenerator>) => ({
     type: GeneratorActionTypes.UPDATE_STATE_FROM_FILE,
     payload: {
       fileState
     }
-  }
-}
+})
 
-export const addToHistory = (tableRollResult: string) => {
-  return {
+export const addToHistory = (tableRollResult: string) => ({
     type: GeneratorActionTypes.ADD_TO_HISTORY,
     payload: {
       tableRollResult
     }
-  }
-}
+})
 
-export const clearHistory = () => {
-  return {
+export const clearHistory = () => ({
     type: GeneratorActionTypes.CLEAR_HISTORY,
-  }
+})
+
+interface addConditionalOperationAttributes {
+  currentTableIndex: number,
+  rollIndexes: number[],
+  nextTableIndex: number,
 }
+export const addConditionalOperation = (operationAttributes: addConditionalOperationAttributes) => ({
+  type: GeneratorActionTypes.ADD_CONDITIONAL_OPERATION,
+  payload: { details: { ...operationAttributes } }
+});
+
+export const removeConditionalOperation = (operationIndex: number) => ({
+  type: GeneratorActionTypes.REMOVE_CONDITIONAL_OPERATION,
+  payload: {
+    operationIndex,
+  }
+});
+
+export const changeConditionalOperation = (operationIndex: number, operationAttributes: addConditionalOperationAttributes) => ({
+  type: GeneratorActionTypes.CHANGE_CONDITIONAL_OPERATION,
+  payload: {
+    operationIndex,
+    details: { ...operationAttributes }
+  }
+});
