746ae917c967d0568249d5fef1c5391234c18f4b
lhawes <lucashawes@Lucass-MacBook-Pro.local>
2021-01-22 12:22:47 -0800

    build new version of app

---
+++ precache-manifest.f74ff71dc42b999fe50f190881742e71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f15b15640ef3a8151a837ecc921f24",
+    "url": "/trpg-table-roller/index.html"
+  },
+  {
+    "revision": "e16b4faabb3f31e887c4",
+    "url": "/trpg-table-roller/static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "527fc6aee32200b348b4",
+    "url": "/trpg-table-roller/static/js/2.95454e50.chunk.js"
+  },
+  {
+    "revision": "0c9eec1cdaa4dbd73523",
+    "url": "/trpg-table-roller/static/js/3.e77f94ad.chunk.js"
+  },
+  {
+    "revision": "e16b4faabb3f31e887c4",
+    "url": "/trpg-table-roller/static/js/main.b20e1977.chunk.js"
+  },
+  {
+    "revision": "452cf2cf2c9cc59316cc",
+    "url": "/trpg-table-roller/static/js/runtime-main.9ad9e488.js"
+  }
+]);
